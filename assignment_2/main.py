61f3b7a44f7da98374173142a5bb2899ed528f83
Lilly <x.ezoza06@gmail.com>
2026-04-21 20:23:21 +0500

    Remove assignment_2 folder

---
+++ assignment_2/main.py
@@ -0,0 +1,12 @@
+# import csv
+# def read_csv(path):
+#     with open(path, 'r') as file:
+#         data=csv.reader(file)
+#         for i in data:
+#             print(i)
+# def write_csv(path, new):
+#     with open(path, 'w', newline='') as file:
+#         data=csv.writer(file)
+#         data.writerow(new)
+# def append(path, new):
+#     with open(path, )
